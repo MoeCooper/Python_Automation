5c4986dd004a31f180d2ac32efba528254305afe
Maurice Cooper <jc70240@gmail.com>
2020-04-12 03:55:37 -0500

    first commit

---
+++ automation.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+
+chrome_browser = webdriver.Chrome('./chromedriver.exe')
+
+chrome_browser.maximize_window()
+chrome_browser.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
+
+assert 'Selenium Easy Demo' in chrome_browser.title
+show_message_button = chrome_browser.find_element_by_class_name("btn-default")
+print(show_message_button.get_attribute('innerHTML'))
+
+assert 'Show Message' in chrome_browser.page_source
+
+user_message = chrome_browser.find_element_by_id('user-message')
+
+user_message.clear()
+
+user_message.send_keys('I am cool!')
+
+show_message_button.click()
+
+output_message = chrome_browser.find_element_by_id('display')
+
+assert 'I am cool!' in output_message.text
+
+chrome_browser.close()
